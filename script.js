462431ab1eb1de2dc7accb49e044e80b4db9fbe3
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-07-02 20:53:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ const apiUrls = [
 ];
 
 // You can write your code here
+function fetchData(apiUrls) {
+  return Promise.all(apiUrls.map(apiUrls => fetch(apiUrls)))
+}
+function comparePerformance() {
+  const start = Date.now()
+  fetchData(apiUrls)
+  const end = Date.now()
+  const time = end - start
+  document.getElementById("output-all").innerHTML = time
+  start = Date.now()
+  fetchData(apiUrls)
+  end = Date.now()
+  time = end - start
+  document.getElementById("output-any").innerHTML = time
+}
+function displayResults() {
+  comparePerformance()
+}
